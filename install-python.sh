87f3fd0c6ddd188e31b9dbe7ed78948864e977ee
zeropaul <landerspaulzero@gmail.com>
2019-06-15 06:09:38 -0500

    More changes

--- install-python.sh
+++ install-python.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "Install Python3.7.0"
+echo "Install Python3.6.0"
 
 cd ~/
-wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
-tar -xvf Python-3.7.0.tgz
-cd ~/Python-3.7.0 && ./configure
+wget https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tgz
+tar -xvf Python-3.6.0.tgz
+cd ~/Python-3.6.0 && ./configure
 make
 sudo make install
 
--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 
 echo "Updating"
 #sudo apt-get update
-echo "Install OpenSSH"
-#sudo apt-get install -y openssh-server
+
+#echo "Install OpenSSH"
+# sudo apt-get install -y openssh-server
 
 echo "Install Git"
 #sudo apt-get install -y git git-core
@@ -31,15 +32,15 @@ echo "Install Git"
 #done
 
 echo "Install Guake"
-#sudo apt-get install -y guake
+# sudo apt-get install -y guake
 
 echo "Install Chrome"
 # wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb --no-check-certificate
-# sudo dpkg -i google-chrome-stable_current_amd64.deb
-# sudo apt-get install -f
+#sudo dpkg -i google-chrome-stable_current_amd64.deb
+#sudo apt-get install -f
 
 echo "Install Rar"
-# sudo apt-get install unrar
+#sudo apt-get install unrar
 
 echo "Install Mocp"
 #sudo apt-get install -y moc
@@ -57,67 +58,70 @@ echo "Install ZSH"
 #sudo -u $USERNAME which zsh
 #continued..
 
-#mv zePool.zsh-theme .oh-my-zsh/themes
+# mv zePool.zsh-theme .oh-my-zsh/themes
 
 echo "Install Pip"
-#sudo apt-get install -y python-pip
+# sudo apt-get install -y python-pip
 
 echo "Install Python Prerequisites"
 # sudo apt install zlib1g-dev
 # sudo apt-get install libffi-dev
 
+echo "Install Virtualenv"
+
+# sudo apt install virtualenv
 
 echo "Install VirtualenvWrapper"
-#pip install --user virtualenvwrapper
+# pip install --user virtualenvwrapper
 
 echo "Install Powerline Fonts"
-#sudo -u $USERNAME pip install git+git://github.com/Lokaltog/powerline
-#wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
-#sudo mv PowerlineSymbols.otf /usr/share/fonts/
-#sudo fc-cache -vf
-#sudo mv PowerlineSymbols.otf /usr/share/fonts/
+# sudo -u $USERNAME pip install git+git://github.com/Lokaltog/powerline
+# wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+# sudo mv PowerlineSymbols.otf /usr/share/fonts/
+# sudo fc-cache -vf
+# sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
 
 echo "Install Tmux"
-#sudo apt-get install -y tmux
+# sudo apt-get install -y tmux
 #continued...
 
 echo "Install Tree"
-#sudo apt-get install -y tree
+# sudo apt-get install -y tree
 
 echo "Install Checkinstall"
-#sudo apt-get install checkinstall
+# sudo apt-get install checkinstall
 
 echo "Install Curl"
-#sudo apt-get install curl
+# sudo apt-get install curl
 
 echo "Install Vim"
-#sudo apt install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
-#libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-#libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-#python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev 
+# sudo apt install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
+# libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+# libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+# python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev 
 
-#sudo apt remove -y vim vim-runtime gvim
-#sudo apt remove -y vim-tiny vim-common vim-gui-common vim-nox
-#git clone https://github.com/vim/vim.git vim-build
-#cd $HOME/vim-build 
+# sudo apt remove -y vim vim-runtime gvim
+# sudo apt remove -y vim-tiny vim-common vim-gui-common vim-nox
+# git clone https://github.com/vim/vim.git vim-build
+# cd $HOME/vim-build 
 
-#./configure --with-features=huge \
-#            --enable-multibyte \
-#	    --enable-rubyinterp=yes \
-#	    --enable-pythoninterp=yes \
-#	    --with-python-config-dir=/usr/lib/python2.7/config \ # pay attention here check directory correct
-#	    --enable-python3interp=yes \
-#	    --with-python3-config-dir=/usr/lib/python3.5/config \
-#	    --enable-perlinterp=yes \
-#	    --enable-luainterp=yes \
+# ./configure --with-features=huge \
+#             --enable-multibyte \
+# 	    --enable-rubyinterp=yes \
+# 	    --enable-pythoninterp=yes \
+# 	    --with-python-config-dir=/usr/lib/python2.7/config \ # pay attention here check directory correct
+# 	    --enable-python3interp=yes \
+# 	    --with-python3-config-dir=/usr/lib/python3.5/config \
+# 	    --enable-perlinterp=yes \
+# 	    --enable-luainterp=yes \
 #            --enable-gui=gtk2 \
-#            --enable-cscope \
-#	   --prefix=/usr/local
+#             --enable-cscope \
+# 	   --prefix=/usr/local
 
-#make VIMRUNTIMEDIR=/usr/local/share/vim/vim81
-#sudo checkinstall  && cd $HOME
+# make VIMRUNTIMEDIR=/usr/local/share/vim/vim81
+# sudo checkinstall  && cd $HOME
 
 echo "Install Bundle"
-#mkdir -p .vim/bundle && cd .vim/bundle #don't
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# mkdir -p .vim/bundle && cd .vim/bundle #don't
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
